0ea35a709f64a7456b26f07dbeb8931317d605a1
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-08-16 15:21:34 +0530

    DOM projects

---
+++ background_color/script.js
@@ -0,0 +1,12 @@
+const button= document.getElementById('btn')
+
+function changeClr(){
+
+    const red= Math.floor(Math.random() * 256)
+    const green= Math.floor(Math.random() * 256)
+    const blue= Math.floor(Math.random() * 256)
+
+    document.body.style.backgroundColor= `rgb(${red},${green},${blue})`
+}
+
+button.addEventListener('click' , changeClr)
